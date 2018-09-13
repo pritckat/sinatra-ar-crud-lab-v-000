e79fa373be4cc3d3b185e43ec9909ffb8c899620
Katrina Pritchard <pritckat@gmail.com>
2018-09-13 19:35:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
-    post.save
+    @post.save
 
     erb :show
   end
